a7400ccd302d3164ce6beb2612bdb729eaff5d54
angelabenfrej <141182585+angelabenfrej@users.noreply.github.com>
2024-07-15 11:58:13 +0100

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.whisper import logger
+from src.whisper.pipeline.stage01_data_igestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to whisper")
+STAGE_NAME = "Data Ingestion Stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
